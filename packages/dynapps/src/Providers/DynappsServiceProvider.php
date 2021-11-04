c51e7f93ef605a968dc47d87cac6d1cfd9ea583a
adyl2016 <adyl.irfan@gmail.com>
2021-11-04 14:58:43 +0500

    first commit

---
+++ packages/dynapps/src/Providers/DynappsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Adylcodes\Dynapps;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class DynappsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
